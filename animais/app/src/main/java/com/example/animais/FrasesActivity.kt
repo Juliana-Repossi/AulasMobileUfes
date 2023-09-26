af83a5509ce92b3da126e4bba67f5d524b010a95
Juliana <julianarepossi@hotmail.com>
2023-09-26 16:53:05 -0300

    inicio app 2

---
+++ animais/app/src/main/java/com/example/animais/FrasesActivity.kt
@@ -0,0 +1,13 @@
+package com.example.animais
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class FrasesActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_frases)
+
+    }
+}
--- imposto_de_renda/app/src/main/java/com/example/atividade/MainActivity.kt
+++ imposto_de_renda/app/src/main/java/com/example/atividade/MainActivity.kt
@@ -20,21 +20,13 @@ class MainActivity : AppCompatActivity() {
         val buttomCalcular = findViewById<Button>("@+id/button_calculate")
 
         buttomCalcular.setOnClickListener{
-            val salario = editTextSalario.text.toString()
+            val salario = editTextSalario.text.toString().toFloat()
             val gastos = editTextGastos.text.toString()
 
-            if(salario.isNotEmpty() && salario>0){
+            if(salario>0){
                 //calcular o IR
+                
 
-//                var impostoSemGasto = when(salario){
-//                    in 0..1903,98 -> "Está no intervalo de 1 a 10"
-//                    in 1903,98..2826,65 -> "Está no intervalo de 11 a 20"
-//                    in 2826,65..3751,05 -> "Está no intervalo de 21 a 30"
-//                    in 3751,05..4664,68 -> "Está no intervalo de 21 a 30"
-//                    else -> "Não está em nenhum intervalo conhecido"
-
-
-                }
 
             }
         }
